b70a1c1b7089bfaeebdd24107fc0d312e4b1c1c6
Ivan Toselli <ivan.toselli31@gmail.com>
2022-03-16 12:27:52 +0100

    Stp 9: Complete 3 inverte vars..

--- Exe2/assets/js/main.js
+++ Exe2/assets/js/main.js
@@ -7,11 +7,11 @@ let wordInputB = prompt ("Inserire la seconda parola: ");
 let wordAlenght = wordInputA.length;
 let wordBlenght = wordInputB.length;
 if (wordAlenght > wordBlenght ){
-    wordOutput.innerHTML = `<div class="col"> ${(wordInputA)}, ${(wordInputB)}</div>`;
+    wordOutput.innerHTML = `<div class="col"> ${(wordInputB)}, ${(wordInputA)}</div>`;
 } 
 else if (wordAlenght  === wordBlenght){
     wordOutput .innerHTML = `<div class="col"> Le due parole sono lunghe uguale: ${(wordInputA)}, ${(wordInputB)}</div>`;
 } 
 else {
-    wordOutput .innerHTML = `<div class="col">${(wordInputB)}, ${(wordInputA)}</div>`;
+    wordOutput .innerHTML = `<div class="col">${(wordInputA)}, ${(wordInputB)}</div>`;
 }
